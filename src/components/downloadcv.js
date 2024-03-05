8802de4e27274b6d22ea4cfee2d07e9088ecafa0
M5ddie <m5ddieperez@gmail.com>
2024-03-06 07:00:22 +0800

    test href path

--- src/components/downloadcv.js
+++ src/components/downloadcv.js
@@ -2,7 +2,9 @@
 const DownloadCV = () => {
     return (
         <div>
-            <a download href="/m5studio/src/assets/pdf/CV_MadelynPerez.pdf">Download CV</a>
+            {/* <a download href="/m5studio/src/assets/pdf/CV_MadelynPerez.pdf">Download CV</a> */}
+            <a download href="src/assets/pdf/CV_MadelynPerez.pdf">Download CV</a>
+            <a download href="https://github.com/M5ddie/m5studio/blob/master/src/assets/pdf/CV_MadelynPerez.pdf">Download CV2</a>
         </div>
     )
 }
