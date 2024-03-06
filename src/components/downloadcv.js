05a10f644524bf7f3ae4e485583232730461bcaf
M5ddie <m5ddieperez@gmail.com>
2024-03-07 03:06:45 +0800

    working DownloadCV

--- src/components/downloadcv.js
+++ src/components/downloadcv.js
@@ -1,12 +1,23 @@
 
 const DownloadCV = () => {
-    return (
-        <div>
-            {/* <a download href="/m5studio/src/assets/pdf/CV_MadelynPerez.pdf">Download CV</a> */}
-            <a download href="src/assets/pdf/CV_MadelynPerez.pdf">Download CV</a>
-            <a download href="https://github.com/M5ddie/m5studio/blob/master/src/assets/pdf/CV_MadelynPerez.pdf">Download CV2</a>
-        </div>
-    )
+  // This is a helper function that creates a hidden link element
+  // and simulates a click event to download the PDF file
+  const downloadFile = async (fileUrl) => {
+    const response = await fetch(fileUrl);
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'CV_MadelynPerez.pdf';
+    link.click();
+    URL.revokeObjectURL(url);
+  }
+
+  return (
+    <button onClick={() => downloadFile('https://raw.githubusercontent.com/M5ddie/m5studio/master/src/assets/pdf/CV_MadelynPerez.pdf')}>
+      Download PDF
+    </button>
+  )
 }
 
 export default DownloadCV
